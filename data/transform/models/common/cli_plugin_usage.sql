5a0a94da32d79f4e6ba41d0fc592759ee5205b88
pnadolny13 <patnadolny@gmail.com>
2021-11-09 11:32:39 -0500

    more env settings refactor to get sqlfluff working

--- data/transform/models/common/cli_plugin_usage.sql
+++ data/transform/models/common/cli_plugin_usage.sql
@@ -1,11 +1,11 @@
 -- ELT Taps
 SELECT
-  event_date,
-  project_id,
-  event_count,
-  SPLIT_PART(command, ' ', 3) AS plugin_name,
-  'tap' AS plugin_type,
-  command_category
+    event_date,
+    project_id,
+    event_count,
+    SPLIT_PART(command, ' ', 3) AS plugin_name,
+    'tap' AS plugin_type,
+    command_category
 FROM {{ ref('stg_ga__cli_events') }}
 WHERE command_category = 'meltano elt'
 
@@ -13,12 +13,12 @@ UNION ALL
 
 -- ELT Targets
 SELECT
-  event_date,
-  project_id,
-  event_count,
-  SPLIT_PART(command, ' ', 4) AS plugin_name,
-  'target' AS plugin_type,
-  command_category
+    event_date,
+    project_id,
+    event_count,
+    SPLIT_PART(command, ' ', 4) AS plugin_name,
+    'target' AS plugin_type,
+    command_category
 FROM {{ ref('stg_ga__cli_events') }}
 WHERE command_category = 'meltano elt'
 
@@ -26,26 +26,26 @@ UNION ALL
 
 -- Invoke Taps
 SELECT
-  event_date,
-  project_id,
-  event_count,
-  SPLIT_PART(command, ' ', 3) AS plugin_name,
-  'tap' AS plugin_type,
-  command_category
+    event_date,
+    project_id,
+    event_count,
+    SPLIT_PART(command, ' ', 3) AS plugin_name,
+    'tap' AS plugin_type,
+    command_category
 FROM {{ ref('stg_ga__cli_events') }}
 WHERE command_category = 'meltano invoke'
-  AND SPLIT_PART(command, ' ', 3) LIKE 'tap%'
+    AND SPLIT_PART(command, ' ', 3) LIKE 'tap%'
 
 UNION ALL
 
 -- Invoke Targets
 SELECT
-  event_date,
-  project_id,
-  event_count,
-  SPLIT_PART(command, ' ', 3) AS plugin_name,
-  'target' AS plugin_type,
-  command_category
+    event_date,
+    project_id,
+    event_count,
+    SPLIT_PART(command, ' ', 3) AS plugin_name,
+    'target' AS plugin_type,
+    command_category
 FROM {{ ref('stg_ga__cli_events') }}
 WHERE command_category = 'meltano invoke'
-  AND SPLIT_PART(command, ' ', 3) LIKE 'target%'
+    AND SPLIT_PART(command, ' ', 3) LIKE 'target%'
--- data/transform/models/marts/meltano_hub/hub_metrics_audit.sql
+++ data/transform/models/marts/meltano_hub/hub_metrics_audit.sql
@@ -1,12 +1,12 @@
 SELECT
-  MAX(event_date) AS updated_date,
-  'meltano_metrics' AS metric_type
+    MAX(event_date) AS updated_date,
+    'meltano_metrics' AS metric_type
 FROM {{ ref('cli_plugin_usage') }}
 
 UNION ALL
 
 SELECT
-  -- TODO: cast to timestamp in stage model
-  MAX(DATE_PARSE(batch_timestamp, '%Y-%m-%d %H:%i:%s.%f')) AS updated_date,
-  'github_metrics' AS metric_type
+    -- TODO: cast to timestamp in stage model
+    MAX(DATE_PARSE(batch_timestamp, '%Y-%m-%d %H:%i:%s.%f')) AS updated_date,
+    'github_metrics' AS metric_type
 FROM {{ source('hub_meltano', 'fact_repo_metrics') }}
