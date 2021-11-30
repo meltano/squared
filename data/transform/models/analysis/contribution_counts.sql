50938523b8ac2906d43485006e10d6312c7ca98a
pnadolny13 <patnadolny@gmail.com>
2021-11-30 15:09:43 -0500

    fix sqlfluff lint error

--- data/transform/models/analysis/contribution_counts.sql
+++ data/transform/models/analysis/contribution_counts.sql
@@ -1,6 +1,6 @@
 SELECT
     COUNT(*) AS contribution_count,
-    DATE_TRUNC('month', contributions.created_at) AS created_at_month
+    DATE_TRUNC('month', created_at) AS created_at_month
 FROM {{ ref('contributions') }}
 WHERE NOT is_team_contribution
 GROUP BY 2
