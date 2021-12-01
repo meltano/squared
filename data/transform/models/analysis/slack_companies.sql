0a4a67b68bd70c9c3178941a39d54fe6fcd1750d
pnadolny13 <patnadolny@gmail.com>
2021-12-01 18:02:04 -0500

    fix analysis query

--- data/transform/models/analysis/slack_companies.sql
+++ data/transform/models/analysis/slack_companies.sql
@@ -1,6 +1,12 @@
-SELECT DISTINCT COALESCE(company_name, email_domain) AS company_name
-FROM {{ ref('stg_slack__users') }}
-WHERE company_name
+WITH parsed AS (
+
+    SELECT DISTINCT COALESCE(company_name, email_domain) AS companies
+    FROM {{ ref('stg_slack__users') }}
+
+)
+
+SELECT * FROM parsed
+WHERE companies
     NOT IN (
         'gmail.com',
         'meltano.com',
