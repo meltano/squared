05b9235ed9d0db0acf745cb42d1c04163515ef0e
pnadolny13 <patnadolny@gmail.com>
2022-02-02 10:40:30 -0500

    fix sums of project event buckets

--- data/transform/models/marts/telemetry/fact_cli_projects.sql
+++ data/transform/models/marts/telemetry/fact_cli_projects.sql
@@ -6,20 +6,20 @@ SELECT
     SUM(event_count) AS events_total,
     COUNT(DISTINCT command) AS unique_commands,
     COUNT(DISTINCT command_category) AS unique_command_categories,
-    SUM(CASE WHEN is_plugin_dbt THEN 1 ELSE 0 END) AS dbt_event_total,
-    SUM(CASE WHEN is_plugin_singer THEN 1 ELSE 0 END) AS singer_event_total,
-    SUM(CASE WHEN is_plugin_airflow THEN 1 ELSE 0 END) AS airflow_event_total,
-    SUM(CASE WHEN is_plugin_dagster THEN 1 ELSE 0 END) AS dagster_event_total,
+    SUM(CASE WHEN is_plugin_dbt THEN event_count ELSE 0 END) AS dbt_event_total,
+    SUM(CASE WHEN is_plugin_singer THEN event_count ELSE 0 END) AS singer_event_total,
+    SUM(CASE WHEN is_plugin_airflow THEN event_count ELSE 0 END) AS airflow_event_total,
+    SUM(CASE WHEN is_plugin_dagster THEN event_count ELSE 0 END) AS dagster_event_total,
     SUM(
-        CASE WHEN is_plugin_lightdash THEN 1 ELSE 0 END
+        CASE WHEN is_plugin_lightdash THEN event_count ELSE 0 END
     ) AS lightdash_event_total,
-    SUM(CASE WHEN is_plugin_superset THEN 1 ELSE 0 END) AS superset_event_total,
-    SUM(CASE WHEN is_plugin_sqlfluff THEN 1 ELSE 0 END) AS sqlfluff_event_total,
+    SUM(CASE WHEN is_plugin_superset THEN event_count ELSE 0 END) AS superset_event_total,
+    SUM(CASE WHEN is_plugin_sqlfluff THEN event_count ELSE 0 END) AS sqlfluff_event_total,
     SUM(
-        CASE WHEN is_os_feature_environments THEN 1 ELSE 0 END
+        CASE WHEN is_os_feature_environments THEN event_count ELSE 0 END
     ) AS environments_event_total,
-    SUM(CASE WHEN is_os_feature_test THEN 1 ELSE 0 END) AS test_event_total,
-    SUM(CASE WHEN is_os_feature_run THEN 1 ELSE 0 END) AS run_event_total,
+    SUM(CASE WHEN is_os_feature_test THEN event_count ELSE 0 END) AS test_event_total,
+    SUM(CASE WHEN is_os_feature_run THEN event_count ELSE 0 END) AS run_event_total,
     COALESCE(SUM(
         event_count
     ) = 1 AND MAX(command_category) = 'meltano init',
