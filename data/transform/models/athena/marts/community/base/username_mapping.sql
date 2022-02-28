d680cfef1fa93f8f9e1d3a0f19f14022f0ebc302
pnadolny13 <patnadolny@gmail.com>
2022-02-28 09:12:04 -0500

    sqlfluff updates

--- data/transform/models/athena/marts/community/base/username_mapping.sql
+++ data/transform/models/athena/marts/community/base/username_mapping.sql
@@ -1,34 +1,68 @@
-with github as (
-    select distinct author_id, author_username from dbt_prod.stg_github__issues
-    union
-    select distinct author_id, author_username from dbt_prod.stg_github__pull_requests 
-),
-gitlab as (
+WITH github AS (
+    SELECT DISTINCT
+        author_id,
+        author_username
+    FROM dbt_prod.stg_github__issues
+    UNION DISTINCT
+    SELECT DISTINCT
+        author_id,
+        author_username
 
-	select distinct author_id, author_username from {{ ref('stg_gitlab__merge_requests') }}
-	union
-	select distinct author_id, author_username from {{ ref('stg_gitlab__issues') }}
+    FROM dbt_prod.stg_github__pull_requests
+),
+
+gitlab AS (
+
+    SELECT DISTINCT
+        author_id,
+        author_username
+    FROM {{ ref('stg_gitlab__merge_requests') }}
+    UNION DISTINCT
+    SELECT DISTINCT
+        author_id,
+        author_username
+    FROM {{ ref('stg_gitlab__issues') }}
 
 ),
-name_matches as (
-select
-    github.author_id AS github_author_id,
-    github.author_username AS github_author_username,
-    gitlab.author_id AS gitlab_author_id,
-    gitlab.author_username AS gitlab_author_username
-from github
-full join gitlab on lower(github.author_username) = lower(gitlab.author_username)
+
+name_matches AS (
+    SELECT
+        github.author_id AS github_author_id,
+        github.author_username AS github_author_username,
+        gitlab.author_id AS gitlab_author_id,
+        gitlab.author_username AS gitlab_author_username
+    FROM github
+    FULL JOIN
+        gitlab ON LOWER(github.author_username) = LOWER(gitlab.author_username)
 ),
-blend_all as (
-select
-    COALESCE(name_matches.github_author_id, manual_contributor_id_mapping.github_id) AS github_author_id,
-    COALESCE(name_matches.github_author_username, manual_contributor_id_mapping.github_username) AS github_author_username,
-    COALESCE(name_matches.gitlab_author_id, manual_contributor_id_mapping.gitlab_id) AS gitlab_author_id,
-    COALESCE(name_matches.gitlab_author_username, manual_contributor_id_mapping.gitlab_username) AS gitlab_author_username
-from name_matches
-left join {{ ref('contributor_id_mapping') }} on name_matches.github_author_id = manual_contributor_id_mapping.github_id
+
+blend_all AS (
+    SELECT
+        COALESCE(
+            name_matches.github_author_id,
+            contributor_id_mapping.github_id
+        ) AS github_author_id,
+        COALESCE(
+            name_matches.github_author_username,
+            contributor_id_mapping.github_username
+        ) AS github_author_username,
+        COALESCE(
+            name_matches.gitlab_author_id,
+            contributor_id_mapping.gitlab_id
+        ) AS gitlab_author_id,
+        COALESCE(
+            name_matches.gitlab_author_username,
+            contributor_id_mapping.gitlab_username
+        ) AS gitlab_author_username
+    FROM name_matches
+    LEFT JOIN
+        {{ ref('contributor_id_mapping') }} ON
+            name_matches.github_author_id = contributor_id_mapping.github_id
 )
-select
-    COALESCE(cast(github_author_id as varchar), '') || COALESCE(cast(gitlab_author_id as varchar), '') AS user_surrogate_key,
-    *
-from blend_all
+
+SELECT
+    *,
+    COALESCE(
+        CAST(github_author_id AS VARCHAR), ''
+    ) || COALESCE(CAST(gitlab_author_id AS VARCHAR), '') AS user_surrogate_key
+FROM blend_all
