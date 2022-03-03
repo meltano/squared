c74bc305dbc0446995a09d79ed43bd8aa9b789f2
pnadolny13 <patnadolny@gmail.com>
2022-03-03 18:44:48 -0500

    convert great expectations to snowflake

--- data/transform/models/publish/meltano_hub/fact_repo_metrics.sql
+++ data/transform/models/publish/meltano_hub/fact_repo_metrics.sql
@@ -1,3 +1,14 @@
+WITH repos AS (
+    -- Dedupe repos with different ids but same full_repo_name. Maybe from migrating a repo.
+    SELECT
+        *,
+        ROW_NUMBER() OVER (
+            PARTITION BY repo_full_name
+            ORDER BY created_at_ts DESC
+        ) AS row_num
+    FROM {{ ref('stg_github_search__repositories') }}
+    WHERE connector_type = 'tap'
+)
 SELECT *
-FROM {{ ref('stg_github_search__repositories') }}
-WHERE connector_type = 'tap'
+FROM repos
+WHERE row_num = 1
