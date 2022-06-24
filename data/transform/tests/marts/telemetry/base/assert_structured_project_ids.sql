f51c56d53ed5dcb621faec7fe24ae98c8a335f0e
pnadolny13 <patnadolny@gmail.com>
2022-06-24 16:52:03 -0400

    fix dbt test bug where project id only found in unstruct set

--- data/transform/tests/marts/telemetry/base/assert_structured_project_ids.sql
+++ data/transform/tests/marts/telemetry/base/assert_structured_project_ids.sql
@@ -2,4 +2,6 @@ SELECT project_id
 FROM {{ ref('stg_ga__cli_events') }}
 WHERE project_id NOT IN (
     SELECT DISTINCT project_id FROM {{ ref('structured_events') }}
+    UNION
+    SELECT DISTINCT project_id FROM {{ ref('unstructured_executions') }}
 )
