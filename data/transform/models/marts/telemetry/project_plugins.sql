f7644ef2d2fb62862955cc86f9a472bbe70fdacf
pnadolny13 <patnadolny@gmail.com>
2022-06-24 15:38:19 -0400

    fix group by errors due to linting fixes

--- data/transform/models/marts/telemetry/project_plugins.sql
+++ data/transform/models/marts/telemetry/project_plugins.sql
@@ -1,37 +1,37 @@
 WITH base_week AS (
     SELECT
         project_id,
-        COUNT(DISTINCT plugin_category) AS plugin_cnt,
-        DATE_TRUNC('week', event_ts) AS week_date
+        DATE_TRUNC('week', event_ts) AS week_date,
+        COUNT(DISTINCT plugin_category) AS plugin_cnt
     FROM {{ ref('fact_plugin_usage') }}
-    GROUP BY 2, 3
+    GROUP BY 1, 2
 ),
 
 base_month AS (
     SELECT
         project_id,
-        COUNT(DISTINCT plugin_category) AS plugin_cnt,
-        DATE_TRUNC('month', event_ts) AS month_date
+        DATE_TRUNC('month', event_ts) AS month_date,
+        COUNT(DISTINCT plugin_category) AS plugin_cnt
     FROM {{ ref('fact_plugin_usage') }}
-    GROUP BY 2, 3
+    GROUP BY 1, 2
 )
 
 SELECT
+    week_date AS agg_period_date,
+    'week' AS agg_period,
     COUNT(project_id) AS total_projects,
     SUM(plugin_cnt) AS plugin_score,
-    AVG(plugin_cnt) AS avg_plugin_per_proj,
-    week_date AS agg_period_date,
-    'week' AS agg_period
+    AVG(plugin_cnt) AS avg_plugin_per_proj
 FROM base_week
-GROUP BY 4, 5
+GROUP BY 1, 2
 
 UNION ALL
 
 SELECT
+    month_date AS agg_period_date,
+    'month' AS agg_period,
     COUNT(project_id) AS total_projects,
     SUM(plugin_cnt) AS plugin_score,
-    AVG(plugin_cnt) AS avg_plugin_per_proj,
-    month_date AS agg_period_date,
-    'month' AS agg_period
+    AVG(plugin_cnt) AS avg_plugin_per_proj
 FROM base_month
-GROUP BY 4, 5
+GROUP BY 1, 2
