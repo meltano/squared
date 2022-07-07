7a8c8554267806148691244f522d8cf612cf70db
pnadolny13 <patnadolny@gmail.com>
2022-07-07 13:14:48 -0400

    Squashed commit of the following:
    commit 6a707a45fa6fd46adff6b10919c9798dda092e7a
    Author: pnadolny13 <patnadolny@gmail.com>
    Date:   Thu Jul 7 13:14:00 2022 -0400
    
        fix plugin usage join and schema tests for project

--- data/transform/models/marts/telemetry/fact_plugin_usage.sql
+++ data/transform/models/marts/telemetry/fact_plugin_usage.sql
@@ -4,3 +4,4 @@ SELECT
     project_dim.is_active AS project_is_active
 FROM {{ ref('plugin_executions') }}
 LEFT JOIN {{ ref('project_dim') }}
+    ON plugin_executions.project_id = project_dim.project_id
