038bd32b3c9cd533654ca685b776ae2c21a0b2d2
pnadolny13 <patnadolny@gmail.com>
2022-07-07 13:34:58 -0400

    project plugins based on new feedback

--- data/transform/models/marts/telemetry/project_plugins.sql
+++ data/transform/models/marts/telemetry/project_plugins.sql
@@ -1,37 +1,54 @@
-WITH base_week AS (
-    SELECT
-        project_id,
-        DATE_TRUNC('week', event_ts) AS week_date,
-        COUNT(DISTINCT plugin_category) AS plugin_cnt
-    FROM {{ ref('fact_plugin_usage') }}
-    GROUP BY 1, 2
+WITH plugins AS (
+    SELECT DISTINCT
+        plugin_executions.event_ts::DATE AS date_day,
+        plugin_executions.project_id,
+        CASE
+            WHEN
+                plugin_executions.plugin_category NOT IN (
+                    'singer', 'dbt', 'great_expectations', 'superset', 'airflow'
+                ) THEN 'other'
+            ELSE plugin_executions.plugin_category
+        END AS plugin_category
+    FROM {{ ref('plugin_executions') }}
+    LEFT JOIN {{ ref('execution_dim') }}
+        ON plugin_executions.execution_id = execution_dim.execution_id
+    LEFT JOIN {{ ref('project_dim') }}
+        ON plugin_executions.project_id = project_dim.project_id
+    WHERE
+        execution_dim.is_exec_event
+        AND DATEDIFF(
+            'day',
+            project_dim.first_event_at::TIMESTAMP,
+            plugin_executions.event_ts::DATE
+        ) >= 7
+
 ),
 
-base_month AS (
+plugins_per_project AS (
     SELECT
-        project_id,
-        DATE_TRUNC('month', event_ts) AS month_date,
-        COUNT(DISTINCT plugin_category) AS plugin_cnt
-    FROM {{ ref('fact_plugin_usage') }}
-    GROUP BY 1, 2
+        date_dim.date_day,
+        (
+            SELECT COUNT(DISTINCT project_id)
+            FROM plugins
+            WHERE plugins.date_day BETWEEN DATEADD(
+                    DAY, -14, date_dim.date_day
+                ) AND date_dim.date_day
+        ) AS projects,
+        (
+            SELECT COUNT(DISTINCT project_id, plugin_category)
+            FROM plugins
+            WHERE plugins.date_day BETWEEN DATEADD(
+                    DAY, -14, date_dim.date_day
+                ) AND date_dim.date_day
+        ) AS categories
+    FROM {{ ref('date_dim') }}
+    WHERE date_dim.date_day <= CURRENT_TIMESTAMP()
+    HAVING projects > 0
 )
 
 SELECT
-    week_date AS agg_period_date,
-    'week' AS agg_period,
-    COUNT(project_id) AS total_projects,
-    SUM(plugin_cnt) AS plugin_score,
-    AVG(plugin_cnt) AS avg_plugin_per_proj
-FROM base_week
-GROUP BY 1, 2
-
-UNION ALL
-
-SELECT
-    month_date AS agg_period_date,
-    'month' AS agg_period,
-    COUNT(project_id) AS total_projects,
-    SUM(plugin_cnt) AS plugin_score,
-    AVG(plugin_cnt) AS avg_plugin_per_proj
-FROM base_month
-GROUP BY 1, 2
+    date_day,
+    projects,
+    categories,
+    categories / projects AS avg_plugin_per_project
+FROM plugins_per_project
