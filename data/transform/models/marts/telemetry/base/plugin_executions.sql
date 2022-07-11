f51112b9d2441851fbcbc5d195f214e19fdef791
pnadolny13 <patnadolny@gmail.com>
2022-07-11 11:11:38 -0400

    consistently pull executions from a base plugin ex table

--- data/transform/models/marts/telemetry/base/plugin_executions.sql
+++ data/transform/models/marts/telemetry/base/plugin_executions.sql
@@ -1,96 +1,72 @@
-WITH base AS (
-    SELECT
-        unstructured_executions.*,
-        plugin.value AS plugin_details
-    FROM {{ ref('unstructured_executions') }},
-        LATERAL FLATTEN(input => plugins) AS plugin_list, -- noqa: L025, L031
-        LATERAL FLATTEN(
-            input => plugin_list.value::VARIANT
-        ) AS plugin -- noqa: L031
-)
-
-SELECT DISTINCT
-    {{ dbt_utils.surrogate_key(
-        [
-            'unstruct_plugins.plugin_surrogate_key',
-            'base.execution_id'
-        ]
-    ) }} AS plugin_exec_pk,
-    base.execution_id,
-    base.started_ts AS event_ts,
+SELECT
+    unstruct_plugin_executions.unstruct_plugin_exec_pk AS plugin_exec_pk,
+    unstruct_plugin_executions.execution_id,
+    unstruct_plugin_executions.started_ts AS event_ts,
     1 AS event_count,
-    base.event_source,
+    unstruct_plugin_executions.event_source,
     'unstructured' AS event_type,
-    base.cli_command,
-    base.struct_command AS full_struct_command,
-    base.struct_command_category,
+    unstruct_plugin_executions.cli_command,
+    unstruct_plugin_executions.struct_command AS full_struct_command,
+    unstruct_plugin_executions.struct_command_category,
     -- plugins
-    unstruct_plugins.plugin_name AS plugin_name,
-    unstruct_plugins.parent_name AS parent_name,
-    unstruct_plugins.executable AS executable,
-    unstruct_plugins.namespace AS namespace,
-    unstruct_plugins.pip_url AS pip_url,
-    unstruct_plugins.variant_name AS plugin_variant,
-    unstruct_plugins.command AS plugin_command,
-    unstruct_plugins.plugin_type,
-    unstruct_plugins.plugin_category,
+    unstruct_plugin_executions.plugin_name AS plugin_name,
+    unstruct_plugin_executions.parent_name AS parent_name,
+    unstruct_plugin_executions.executable AS executable,
+    unstruct_plugin_executions.namespace AS namespace,
+    unstruct_plugin_executions.pip_url AS pip_url,
+    unstruct_plugin_executions.variant_name AS plugin_variant,
+    unstruct_plugin_executions.command AS plugin_command,
+    unstruct_plugin_executions.plugin_type,
+    unstruct_plugin_executions.plugin_category,
+    unstruct_plugin_executions.plugin_surrogate_key,
     -- projects
-    base.project_id,
+    unstruct_plugin_executions.project_id,
     -- environments
-    base.environment_name_hash AS env_id,
+    unstruct_plugin_executions.environment_name_hash AS env_id,
     hash_lookup.unhashed_value AS env_name,
     -- executions
-    base.exit_code AS cli_execution_exit_code,
-    base.process_duration_ms AS cli_execution_time_ms,
+    unstruct_plugin_executions.exit_code AS cli_execution_exit_code,
+    unstruct_plugin_executions.process_duration_ms AS cli_execution_time_ms,
     -- random
-    base.user_ipaddress,
-    base.meltano_version,
-    base.num_cpu_cores_available,
-    base.windows_edition,
-    base.machine,
-    base.system_release,
-    base.freedesktop_id,
-    base.freedesktop_id_like,
-    base.is_dev_build,
-    base.process_hierarchy,
-    base.python_version,
-    base.client_uuid,
-    base.is_ci_environment,
-    base.num_cpu_cores,
-    base.python_implementation,
-    base.system_name,
-    base.system_version,
-    base.exception_type AS cli_exception_type,
-    base.exception_cause AS cli_exception_cause,
-    base.event_states,
-    base.event_block_types
-FROM base
-LEFT JOIN {{ ref('unstruct_plugins') }}
-    ON unstruct_plugins.plugin_surrogate_key = {{ dbt_utils.surrogate_key(
-        ['base.plugin_details']
-    ) }}
+    unstruct_plugin_executions.user_ipaddress,
+    unstruct_plugin_executions.meltano_version,
+    unstruct_plugin_executions.num_cpu_cores_available,
+    unstruct_plugin_executions.windows_edition,
+    unstruct_plugin_executions.machine,
+    unstruct_plugin_executions.system_release,
+    unstruct_plugin_executions.freedesktop_id,
+    unstruct_plugin_executions.freedesktop_id_like,
+    unstruct_plugin_executions.is_dev_build,
+    unstruct_plugin_executions.process_hierarchy,
+    unstruct_plugin_executions.python_version,
+    unstruct_plugin_executions.client_uuid,
+    unstruct_plugin_executions.is_ci_environment,
+    unstruct_plugin_executions.num_cpu_cores,
+    unstruct_plugin_executions.python_implementation,
+    unstruct_plugin_executions.system_name,
+    unstruct_plugin_executions.system_version,
+    unstruct_plugin_executions.exception_type AS cli_exception_type,
+    unstruct_plugin_executions.exception_cause AS cli_exception_cause,
+    unstruct_plugin_executions.event_states,
+    unstruct_plugin_executions.event_block_types
+FROM {{ ref('unstruct_plugin_executions') }}
 LEFT JOIN {{ ref('hash_lookup') }}
-    ON base.environment_name_hash = hash_lookup.hash_value
+    ON unstruct_plugin_executions.environment_name_hash = hash_lookup.hash_value
 
 UNION ALL
 
 SELECT
-    {{ dbt_utils.surrogate_key(
-        [
-            'plugins_cmd_map.plugin_name',
-            'structured_executions.execution_id'
-        ]
-    ) }} AS plugin_exec_pk,
-    structured_executions.execution_id,
-    structured_executions.event_created_at AS event_ts,
-    structured_executions.event_count AS event_count,
-    structured_executions.event_source,
+    struct_plugin_executions.struct_plugin_exec_pk AS plugin_exec_pk,
+    struct_plugin_executions.execution_id,
+    struct_plugin_executions.event_created_at AS event_ts,
+    struct_plugin_executions.event_count AS event_count,
+    struct_plugin_executions.event_source,
     'structured' AS event_type,
-    SPLIT_PART(cmd_parsed_all.command_category, ' ', 2) AS command,
-    cmd_parsed_all.command AS full_struct_command,
-    cmd_parsed_all.command_category,
+    struct_plugin_executions.command,
+    struct_plugin_executions.command AS full_struct_command,
+    struct_plugin_executions.command_category,
     -- plugins
-    plugins_cmd_map.plugin_name AS plugin_name,
+    struct_plugin_executions.plugin_name,
     NULL AS parent_name,
     NULL AS executable,
     NULL AS namespace,
@@ -98,11 +74,12 @@ SELECT
     NULL AS plugin_variant,
     NULL AS plugin_command,
     NULL AS plugin_type, -- extractor/loader/etc.
-    plugins_cmd_map.plugin_category,
+    struct_plugin_executions.plugin_category,
+    NULL AS plugin_surrogate_key,
     -- projects
-    structured_executions.project_id,
+    struct_plugin_executions.project_id,
     -- environments
-    cmd_parsed_all.environment AS env_id,
+    struct_plugin_executions.env_id,
     hash_lookup.unhashed_value AS env_name,
     -- executions
     0 AS exit_code,
@@ -129,14 +106,6 @@ SELECT
     NULL AS exception_cause,
     NULL AS event_states,
     NULL AS event_block_types
-FROM {{ ref('structured_executions') }}
-LEFT JOIN
-    {{ ref('cmd_parsed_all') }} ON
-        structured_executions.command = cmd_parsed_all.command
+FROM {{ ref('struct_plugin_executions') }}
 LEFT JOIN {{ ref('hash_lookup') }}
-    ON cmd_parsed_all.environment = hash_lookup.hash_value
-LEFT JOIN
-    {{ ref('plugins_cmd_map') }} ON
-        structured_executions.command = plugins_cmd_map.command
-WHERE cmd_parsed_all.command_type = 'plugin'
-    AND plugins_cmd_map.plugin_name IS NOT NULL
+    ON struct_plugin_executions.env_id = hash_lookup.hash_value
