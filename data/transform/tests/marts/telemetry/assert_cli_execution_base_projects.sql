541b7eb8a40bd726a40ecc883571c77f595e260d
pnadolny13 <patnadolny@gmail.com>
2022-07-14 08:44:17 -0400

    cli executions to table materialization and test to assert project IDs

---
+++ data/transform/tests/marts/telemetry/assert_cli_execution_base_projects.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(*)
+FROM {{ ref('cli_executions_base') }}
+LEFT JOIN
+    {{ ref('project_dim') }} ON
+        cli_executions_base.project_id = project_dim.project_id
+WHERE project_dim.project_id IS NULL;
