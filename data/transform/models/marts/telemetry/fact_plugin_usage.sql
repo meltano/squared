41eb16ff79aca1f98458b224e707cc62f63cd2ed
pnadolny13 <patnadolny@gmail.com>
2022-07-15 16:11:22 -0400

    remove duplicate column

--- data/transform/models/marts/telemetry/fact_plugin_usage.sql
+++ data/transform/models/marts/telemetry/fact_plugin_usage.sql
@@ -2,7 +2,6 @@ SELECT
     plugin_executions.*,
     project_dim.first_event_at AS project_created_at,
     project_dim.is_active AS project_is_active,
-    ip_address_dim.ip_address_hash,
     ip_address_dim.cloud_provider,
     ip_address_dim.execution_location
 FROM {{ ref('plugin_executions') }}
