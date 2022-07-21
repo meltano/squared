f39b066f43772057b511e4afab6aecfe0264ec1c
pnadolny13 <patnadolny@gmail.com>
2022-07-21 09:07:21 -0400

    unhash only hub defined plugins

--- data/transform/models/marts/telemetry/base/hash_lookup.sql
+++ data/transform/models/marts/telemetry/base/hash_lookup.sql
@@ -1,14 +1,56 @@
 WITH base AS (
-    SELECT DISTINCT
-        plugin_name AS unhashed_value,
-        SHA2_HEX(plugin_name) AS hash_value
-    FROM {{ ref('struct_plugins') }}
 
-    UNION
+    SELECT DISTINCT
+        name AS unhashed_value,
+        SHA2_HEX(name) AS hash_value,
+        'plugin_name' AS category
+    FROM {{ ref('stg_meltanohub__plugins') }}
+
+    UNION ALL
+
+    SELECT DISTINCT
+        namespace AS unhashed_value,
+        SHA2_HEX(namespace) AS hash_value,
+        'plugin_namespace' AS category
+    FROM {{ ref('stg_meltanohub__plugins') }}
+
+    UNION ALL
+
+    SELECT DISTINCT
+        executable AS unhashed_value,
+        SHA2_HEX(executable) AS hash_value,
+        'plugin_executable' AS category
+    FROM {{ ref('stg_meltanohub__plugins') }}
+
+    UNION ALL
+
+    SELECT DISTINCT
+        pip_url AS unhashed_value,
+        SHA2_HEX(pip_url) AS hash_value,
+        'plugin_pip_url' AS category
+    FROM {{ ref('stg_meltanohub__plugins') }}
+
+    UNION ALL
+
+    SELECT DISTINCT
+        variant AS unhashed_value,
+        SHA2_HEX(variant) AS hash_value,
+        'plugin_variant' AS category
+    FROM {{ ref('stg_meltanohub__plugins') }}
+
+    UNION ALL
+
+    SELECT
+        'original' AS unhashed_value,
+        SHA2_HEX('original') AS hash_value,
+        'plugin_variant' AS category
+
+    UNION ALL
 
     SELECT
         f.value::STRING AS unhashed_value,
-        SHA2_HEX(f.value) AS hash_value
+        SHA2_HEX(f.value) AS hash_value,
+        'environment' AS category
     FROM
         TABLE(
             FLATTEN(
@@ -19,28 +61,10 @@ WITH base AS (
             )
         ) AS f
 
-    UNION
-
-    SELECT
-        f.value::STRING AS unhashed_value,
-        SHA2_HEX(f.value) AS hash_value
-    FROM
-        TABLE(
-            FLATTEN(
-                input => PARSE_JSON(
-                    '["apache", "singer-io", "transferwise", "meltano", \
-                    "meltanolabs", "bytecodeio", "fishtown-analytics", \
-                    "Mashey", "hotgluexyz", "Matatika", "Pathlight", \
-                    "dbt-labs", "sqlfluff", "great-expectations", "dataops-tk", \
-                    "anelendata", "AutoIDM", "datateer", "adswerve", \
-                    "coeff", "shrutikaponde-vc", "datadotworld", "andyh1203", \
-                    "prontopro", "estrategiahq"]'
-                )
-            )
-        ) AS f
 )
 
 SELECT DISTINCT
     unhashed_value,
-    hash_value
+    hash_value,
+    category
 FROM base
--- data/transform/models/marts/telemetry/base/structured_parsing/struct_plugins.sql
+++ data/transform/models/marts/telemetry/base/structured_parsing/struct_plugins.sql
@@ -1,23 +1,60 @@
+WITH base_singer AS (
+    SELECT
+        flat.value::STRING AS plugin_name,
+        cmd_parsed_all.command
+    FROM {{ ref('cmd_parsed_all') }},
+        LATERAL FLATTEN(input=>singer_plugins) AS flat
+    WHERE cmd_parsed_all.command_type = 'plugin'
+),
+
+base_other AS (
+    SELECT
+        flat.value::STRING AS plugin_name,
+        cmd_parsed_all.command
+    FROM {{ ref('cmd_parsed_all') }},
+        LATERAL FLATTEN(input=>other_plugins) AS flat
+    WHERE cmd_parsed_all.command_type = 'plugin'
+)
+
 SELECT DISTINCT
-    flat.value::STRING AS plugin_name,
-    cmd_parsed_all.command,
-    'singer' AS plugin_category
-FROM {{ ref('cmd_parsed_all') }},
-    LATERAL FLATTEN(input=>singer_plugins) AS flat
-WHERE cmd_parsed_all.command_type = 'plugin'
+    COALESCE(
+        hash_lookup.unhashed_value,
+        SHA2_HEX(base_singer.plugin_name)
+    ) AS plugin_name,
+    base_singer.command,
+    'singer' AS plugin_category,
+    COALESCE(stg_meltanohub__plugins.plugin_type, 'UNKNOWN') AS plugin_type,
+    NULL AS plugin_command
+FROM base_singer
+LEFT JOIN {{ ref('hash_lookup') }}
+    ON SHA2_HEX(base_singer.plugin_name) = hash_lookup.hash_value
+        AND hash_lookup.category = 'plugin_name'
+LEFT JOIN {{ ref('stg_meltanohub__plugins') }}
+    ON base_singer.plugin_name = stg_meltanohub__plugins.name
+
 -- Mappers are included in the singer_plugins list
 
 UNION ALL
 
 SELECT DISTINCT
-    flat.value::STRING AS plugin_name,
-    cmd_parsed_all.command,
+    COALESCE(
+        hash_lookup.unhashed_value,
+        SHA2_HEX(base_other.plugin_name)
+    ) AS plugin_name,
+    base_other.command,
     CASE
-        WHEN flat.value::STRING LIKE 'dbt-%' THEN 'dbt'
+        WHEN base_other.plugin_name LIKE 'dbt-%' THEN 'dbt'
         ELSE
-            SPLIT_PART(flat.value::STRING, ':', 1)
+            SPLIT_PART(base_other.plugin_name, ':', 1)
     END
-    AS plugin_category
-FROM {{ ref('cmd_parsed_all') }},
-    LATERAL FLATTEN(input=>other_plugins) AS flat
-WHERE cmd_parsed_all.command_type = 'plugin'
+    AS plugin_category,
+    COALESCE(stg_meltanohub__plugins.plugin_type, 'UNKNOWN') AS plugin_type,
+    SPLIT_PART(base_other.plugin_name, ':', 2) AS plugin_command
+FROM base_other
+LEFT JOIN {{ ref('hash_lookup') }}
+    ON SHA2_HEX(
+        SPLIT_PART(base_other.plugin_name, ':', 1)
+    ) = hash_lookup.hash_value
+    AND hash_lookup.category = 'plugin_name'
+LEFT JOIN {{ ref('stg_meltanohub__plugins') }}
+    ON SPLIT_PART(base_other.plugin_name, ':', 1) = stg_meltanohub__plugins.name
