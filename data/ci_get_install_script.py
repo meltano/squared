56c3fb0fdadcd8c6819141442206e6dbc5d1f301
pnadolny13 <patnadolny@gmail.com>
2022-07-21 17:50:21 -0400

    ci script for building install commands

---
+++ data/ci_get_install_script.py
@@ -0,0 +1,20 @@
+import sys
+import json
+
+data = json.load(sys.stdin)
+my_job = sys.argv[1]
+
+script = set()
+for job in data.get('jobs'):
+    if my_job == job.get('job_name'):
+        for task in job.get('tasks'):
+            plugins = task.split(' ')
+            for index, plugin in enumerate(plugins):
+                if ':' in plugin:
+                    plugin = plugin.split(':')[0]
+                    script.add(f'meltano install transformer {plugin};')
+                elif plugin.startswith('tap-'):
+                  script.add(f'meltano install extractor {plugin};')
+                elif len(plugins) > 2 and index not in (0, len(plugins)-1):
+                  script.add(f'meltano install mapper {plugin};')
+print(' '.join(script))
