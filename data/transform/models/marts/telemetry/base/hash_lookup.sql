e67539a0c9535be3824e2620b32ee81e7e2435ce
pnadolny13 <patnadolny@gmail.com>
2022-08-03 16:15:39 -0400

    unhash git pip urls

--- data/transform/models/marts/telemetry/base/hash_lookup.sql
+++ data/transform/models/marts/telemetry/base/hash_lookup.sql
@@ -34,6 +34,14 @@ WITH base AS (
     UNION ALL
 
     SELECT DISTINCT
+        'git+' || repo || '.git' AS unhashed_value,
+        SHA2_HEX('git+' || repo || '.git') AS hash_value,
+        'plugin_pip_url' AS category
+    FROM {{ ref('stg_meltanohub__plugins') }}
+
+    UNION ALL
+
+    SELECT DISTINCT
         variant AS unhashed_value,
         SHA2_HEX(variant) AS hash_value,
         'plugin_variant' AS category
