db0d114c00adffb32f8a6e4ef0d217d3549bdbdf
pnadolny13 <patnadolny@gmail.com>
2022-09-22 11:36:20 -0400

    pull project dim logic to base table to avoid cyclic tree

--- data/transform/models/marts/telemetry/project_dim.sql
+++ data/transform/models/marts/telemetry/project_dim.sql
@@ -4,61 +4,22 @@
 
 WITH active_projects AS (
 
-    SELECT
-        DISTINCT
-        project_id
+    SELECT DISTINCT project_id
     FROM {{ ref('daily_active_projects') }}
     WHERE date_day >= DATEADD(
         'day', -28, CURRENT_DATE()
     )
 
-),
-
-first_project_source AS (
-
-    SELECT DISTINCT
-        project_id,
-        FIRST_VALUE(
-            CASE
-                WHEN cli_command != 'init'
-                    THEN project_uuid_source
-            END
-        ) IGNORE NULLS OVER (
-            PARTITION BY project_id ORDER BY event_created_at ASC
-        ) AS first_source
-    FROM {{ ref('cli_executions_base') }}
-
 )
 
 SELECT
-    cli_executions_base.project_id,
-    MAX(
-        COALESCE(active_projects.project_id IS NOT NULL, FALSE)
-    ) AS is_active,
-    MIN(cli_executions_base.event_created_at) AS first_event_at,
-    MAX(cli_executions_base.event_created_at) AS last_event_at,
-    SUM(
-        CASE
-            WHEN
-                cli_executions_base.is_exec_event
-                THEN cli_executions_base.event_count
-            ELSE 0
-        END
-    ) AS exec_event_total,
-    MAX(
-        COALESCE(
-            COALESCE(
-                first_project_source.first_source,
-                cli_executions_base.project_uuid_source
-            ),
-            'UNKNOWN'
-        )
-    ) AS project_id_source
-FROM {{ ref('cli_executions_base') }}
+    project_base.project_id,
+    project_base.first_event_at,
+    project_base.last_event_at,
+    project_base.exec_event_total,
+    project_base.project_id_source,
+    COALESCE(active_projects.project_id IS NOT NULL, FALSE) AS is_active
+FROM {{ ref('project_base') }}
 LEFT JOIN
     active_projects ON
-        cli_executions_base.project_id = active_projects.project_id
-LEFT JOIN
-    first_project_source ON
-        cli_executions_base.project_id = first_project_source.project_id
-GROUP BY 1
+        project_base.project_id = active_projects.project_id
