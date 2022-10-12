d9e1354d78a25aaec80e15c283849e31e1d5798e
pnadolny13 <patnadolny@gmail.com>
2022-10-11 23:24:35 -0400

    fix bug where we were excluding struct events that have context

--- data/transform/models/marts/telemetry/base/unstructured_parsing/unstruct_event_flattened.sql
+++ data/transform/models/marts/telemetry/base/unstructured_parsing/unstruct_event_flattened.sql
@@ -222,3 +222,62 @@ LEFT JOIN {{ ref('context_project') }}
 -- The original implementation didn't have a project_uuid so they aren't
 -- useful for anything and should be excluded.
 WHERE context_project.project_uuid IS NOT NULL
+
+UNION ALL
+
+SELECT
+    event_legacy_with_context.event_created_at,
+    event_legacy_with_context.user_ipaddress,
+    event_legacy_with_context.event_id,
+    NULL AS event,
+    event_legacy_with_context.event_name,
+    NULL AS block_type,
+    NULL AS exit_code,
+    NULL AS exit_timestamp,
+    NULL AS process_duration_microseconds,
+    NULL AS changed_from,
+    NULL AS changed_to,
+    NULL AS setting_name,
+    context_exception.exception,
+    context_plugins.plugins_obj,
+    SPLIT_PART(
+        event_legacy_with_context.legacy_se_category,
+        ' ',
+        2
+    ) AS cli_command,
+    NULL AS sub_command,
+    context_cli.options_obj,
+    context_environment.freedesktop_version_id,
+    context_environment.machine,
+    context_environment.meltano_version,
+    context_environment.num_cpu_cores_available,
+    context_environment.windows_edition,
+    context_environment.is_dev_build,
+    context_environment.is_ci_environment,
+    context_environment.python_version,
+    context_environment.python_implementation,
+    context_environment.system_name,
+    context_environment.system_release,
+    context_environment.system_version,
+    context_environment.freedesktop_id,
+    context_environment.freedesktop_id_like,
+    context_environment.num_cpu_cores,
+    context_environment.process_hierarchy,
+    context_project.context_uuid,
+    context_project.project_uuid,
+    context_project.project_uuid_source,
+    context_project.environment_name_hash,
+    context_project.client_uuid,
+    context_project.send_anonymous_usage_stats,
+    context_project.send_anonymous_usage_stats_source
+FROM {{ ref('event_legacy_with_context') }}
+LEFT JOIN {{ ref('context_cli') }}
+    ON event_legacy_with_context.event_id = context_cli.event_id
+LEFT JOIN {{ ref('context_plugins') }}
+    ON event_legacy_with_context.event_id = context_plugins.event_id
+LEFT JOIN {{ ref('context_exception') }}
+    ON event_legacy_with_context.event_id = context_exception.event_id
+LEFT JOIN {{ ref('context_environment') }}
+    ON event_legacy_with_context.event_id = context_environment.event_id
+LEFT JOIN {{ ref('context_project') }}
+    ON event_legacy_with_context.event_id = context_project.event_id
