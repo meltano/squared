0b211f2e1c6f1912cd3e2504e30b3af261eeb645
Aaron ("AJ") Steers <aj@meltano.com>
2022-11-04 12:28:56 -0700

    proposed slack post format update

--- data/transform/macros/slack_message_generator.sql
+++ data/transform/macros/slack_message_generator.sql
@@ -1,3 +1,3 @@
 {% macro slack_message_generator() -%}
-'\n     • <' || html_url || ' | ' || organization_name || '/' || repo_name || ' #' || GET(split(html_url, '/'), 6)::STRING || '> - _' || LEFT(title, 25) || '..._ by *' || author_username || '* \n'
+'\n     • <' || html_url || ' | ' || organization_name || '/' || repo_name || ' #' || GET(split(html_url, '/'), 6)::STRING || '> (*' || author_username || '*): _' || title || '_\n'
 {%- endmacro %}
