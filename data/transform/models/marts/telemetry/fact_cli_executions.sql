d54b52888e4af186769311c5f889058415c1da2d
pnadolny13 <patnadolny@gmail.com>
2022-11-22 13:46:01 -0500

    fix sqlfluff issues

--- data/transform/models/marts/telemetry/fact_cli_executions.sql
+++ data/transform/models/marts/telemetry/fact_cli_executions.sql
@@ -159,7 +159,8 @@ SELECT
     project_segments_monthly.monthly_piplines_all,
     project_segments_monthly.monthly_piplines_active,
     project_segments_monthly.monthly_piplines_active_eom,
-    prev_project_segments_monthly.monthly_piplines_active_eom AS monthly_piplines_previous,
+    prev_project_segments_monthly.monthly_piplines_active_eom
+    AS monthly_piplines_previous,
     COALESCE(
         project_segments_monthly.monthly_piplines_all_segment,
         'NO_PIPELINES'
@@ -181,7 +182,8 @@ LEFT JOIN project_segments_monthly
     ON base.project_id = project_segments_monthly.project_id
         AND base.first_day_of_month
         = project_segments_monthly.first_day_of_month
-LEFT JOIN project_segments_monthly AS prev_project_segments_monthly
+LEFT JOIN project_segments_monthly
+    AS prev_project_segments_monthly -- noqa: L031
     ON base.project_id = prev_project_segments_monthly.project_id
-        AND DATEADD(month, -1, base.first_day_of_month)
+        AND DATEADD(MONTH, -1, base.first_day_of_month)
         = prev_project_segments_monthly.first_day_of_month
