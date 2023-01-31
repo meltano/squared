d05af77d708f4df8b60339e9b3154433018a6e6a
pnadolny13 <patnadolny@gmail.com>
2023-01-31 13:22:07 -0500

    add fact table for api requests and refactor base table, add schema tests

--- data/transform/models/marts/telemetry/base/hub_logs_parsed.sql
+++ data/transform/models/marts/telemetry/base/hub_logs_parsed.sql
@@ -1,45 +1,77 @@
-with base as (
-    select
-        case when startswith(log_message, '(') then SUBSTR(log_message, 40, 10000000) else log_message end as payload,
-        case when startswith(log_message, '(') then SUBSTR(log_message, 2, 36) else parse_json(payload):requestId end as request_id,
-        case when startswith(payload, 'Method') then split_part(payload, ':', 1) end as method_type,
-        case when startswith(payload, 'Method') then ARRAY_TO_STRING(ARRAY_SLICE(split(payload, ':'), 1,100000000), ':') end as method_body,
-        REPLACE(split_part(split_part(payload, 'User-Agent=', 2), ',',1), 'Meltano/', '') AS meltano_version,
-     	split_part(split_part(payload, 'X-Project-ID=', 2), ', ', 1) AS project_id,
-     	case when startswith(payload, '{') then parse_json(payload):ip::string end as ip_address,
-        -- TODO: this isnt perfect, still get some other random stuff
-     	case when startswith(payload, 'HTTP Method: GET, Resource Path') then split_part(payload, ': ', 3) end as request_path,
-     	ARRAY_SLICE(
-    		split(
-    			split_part(
-    				split_part(payload, 'X-Forwarded-For=', 2),
-    				'=', 1
-    			), ', '
-    		),
-    		0, -1
-    	) AS ip_address_array,
-        *
-    from {{ ref('stg_cloudwatch__log')}}
-),
-request_rollup as (
-    select
-        request_id,
-        max(meltano_version) as meltano_version,
-        max(project_id) as project_id,
-        max(ip_address) as ip_address
-    from base
-    group by 1
-
+WITH base AS (
+    SELECT
+        log_message,
+        created_at_ts,
+        CASE
+            WHEN
+                startswith(
+                    log_message, '('
+                ) THEN substr(log_message, 40, 10000000)
+            ELSE log_message
+        END AS payload,
+        CASE
+            WHEN
+                startswith(log_message, '(') THEN substr(log_message, 2, 36)
+            ELSE parse_json(payload):requestId
+        END AS request_id,
+        CASE
+            WHEN startswith(payload, 'Method') THEN split_part(payload, ':', 1)
+        END AS method_type,
+        CASE
+            WHEN
+                startswith(
+                    payload, 'Method'
+                ) THEN array_to_string(
+                    array_slice(split(payload, ':'), 1, 100000000), ':'
+                )
+        END AS method_body,
+        split_part(split_part(payload, 'User-Agent=', 2), ',', 1) AS user_agent,
+        CASE
+            WHEN method_type = 'Method completed with status' THEN method_body
+        END AS status_code,
+        CASE
+            WHEN
+                payload LIKE 'Received response.%' THEN split_part(
+                    split_part(payload, 'Integration latency: ', 2), ' ms', 1
+                )
+        END AS integration_latency_ms,
+        CASE
+            WHEN
+                user_agent LIKE 'Meltano%' THEN replace(
+                    user_agent, 'Meltano/', ''
+                )
+        END AS meltano_version,
+        split_part(
+            split_part(payload, 'X-Meltano-Command=', 2), ',', 1
+        ) AS command,
+        split_part(
+            split_part(payload, 'X-Project-ID=', 2), ', ', 1
+        ) AS project_id,
+        CASE
+            WHEN startswith(payload, '{') THEN parse_json(payload):ip::string
+        END AS ip_address,
+        CASE
+            WHEN
+                startswith(
+                    payload, 'HTTP Method: GET, Resource Path'
+                ) THEN split_part(payload, ': ', 3)
+        END AS request_path
+    FROM {{ ref('stg_cloudwatch__log') }}
 )
-select
-    base.payload,
-    base.request_id,
-    base.method_type,
-    base.method_body,
-    base.request_path,
-    request_rollup.meltano_version,
-    request_rollup.project_id,
-    request_rollup.ip_address,
-    base.log_message
-from base
-    left join request_rollup on base.request_id = request_rollup.request_id
+
+SELECT
+    request_id,
+    payload,
+    method_type,
+    method_body,
+    user_agent,
+    integration_latency_ms,
+    status_code,
+    request_path,
+    meltano_version,
+    command,
+    project_id,
+    log_message,
+    created_at_ts,
+    md5(ip_address) AS ip_address_hash
+FROM base
