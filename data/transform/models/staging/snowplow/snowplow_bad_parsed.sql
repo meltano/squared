6c477cbf38e76b61c92f809e61eee985903a5870
Pat Nadolny <patnadolny@gmail.com>
2023-02-03 23:27:22 -0500

    Reparse more bad events (#545)

--- data/transform/models/staging/snowplow/snowplow_bad_parsed.sql
+++ data/transform/models/staging/snowplow/snowplow_bad_parsed.sql
@@ -13,9 +13,50 @@ WITH reparse_1 AS (
         ]:schemaKey::string
         = 'iglu:com.meltano/environment_context/jsonschema/1-2-0'
 
+),
+
+reparse_2 AS (
+    -- Incident: exit_event incorrectly required not null exit_code
+    -- Affected versions 2.0.3 - 2.6.0
+    -- Related issues: https://github.com/meltano/meltano/issues/6243 and
+    -- https://github.com/meltano/meltano/pull/6244
+    SELECT
+        payload_enriched,
+        uploaded_at
+    FROM {{ ref('stg_snowplow__events_bad') }}
+    WHERE failure_message[
+            0
+        ]:schemaKey::string
+        = 'iglu:com.meltano/exit_event/jsonschema/1-0-0'
+        AND failure_message[
+            0
+        ]:error:dataReports[0]:message::string
+        = '$.exit_code: null found, integer expected'
+
+),
+
+reparse_3 AS (
+    -- Incident: events sent with schema name exceptions_context vs
+    -- exception_context as registered in Snowcat
+    -- Affected versions 2.0.1 - 2.0.2
+    -- Related issues: https://github.com/meltano/meltano/issues/6213 and
+    -- https://github.com/meltano/meltano/pull/6212
+    SELECT
+        uploaded_at,
+        REPLACE(
+            payload_enriched,
+            'iglu:com.meltano/exceptions_context/jsonschema/1-0-0',
+            'iglu:com.meltano/exception_context/jsonschema/1-0-0'
+        ) AS payload_enriched
+    FROM {{ ref('stg_snowplow__events_bad') }}
+    WHERE failure_message[
+            0
+        ]:schemaKey::string
+        = 'iglu:com.meltano/exceptions_context/jsonschema/1-0-0'
+
 )
 
-{% for cte_name in ['reparse_1'] %}
+{% for cte_name in ['reparse_1', 'reparse_2', 'reparse_3'] %}
 
 {%- if not loop.first %}
 UNION ALL
@@ -161,26 +202,30 @@ SELECT -- noqa: L034
     PARSE_JSON(payload_enriched):derived_contexts::string AS derived_contexts,
     PARSE_JSON(payload_enriched):domain_sessionid::string AS domain_sessionid,
     collector_tstamp AS derived_tstamp,
-    SPLIT_PART(
-        SPLIT_PART(PARSE_JSON(unstruct_event):data:schema::string, ':', 2),
-        '/',
-        1
-    ) AS event_vendor,
-    SPLIT_PART(
-        SPLIT_PART(PARSE_JSON(unstruct_event):data:schema::string, ':', 2),
-        '/',
-        2
-    ) AS event_name,
-    SPLIT_PART(
-        SPLIT_PART(PARSE_JSON(unstruct_event):data:schema::string, ':', 2),
-        '/',
-        3
-    ) AS event_format,
-    SPLIT_PART(
-        SPLIT_PART(PARSE_JSON(unstruct_event):data:schema::string, ':', 2),
-        '/',
-        4
-    ) AS event_version,
+    CASE WHEN unstruct_event IS NULL THEN 'com.google.analytics' ELSE
+        SPLIT_PART(
+            SPLIT_PART(PARSE_JSON(unstruct_event):data:schema::string, ':', 2),
+            '/',
+            1
+        ) END AS event_vendor,
+    CASE WHEN unstruct_event IS NULL THEN 'event' ELSE
+        SPLIT_PART(
+            SPLIT_PART(PARSE_JSON(unstruct_event):data:schema::string, ':', 2),
+            '/',
+            2
+        ) END AS event_name,
+    CASE WHEN unstruct_event IS NULL THEN 'jsonschema' ELSE
+        SPLIT_PART(
+            SPLIT_PART(PARSE_JSON(unstruct_event):data:schema::string, ':', 2),
+            '/',
+            3
+        ) END AS event_format,
+    CASE WHEN unstruct_event IS NULL THEN '1-0-0' ELSE
+        SPLIT_PART(
+            SPLIT_PART(PARSE_JSON(unstruct_event):data:schema::string, ':', 2),
+            '/',
+            4
+        ) END AS event_version,
     PARSE_JSON(payload_enriched):event_fingerprint::string AS event_fingerprint,
     PARSE_JSON(payload_enriched):true_tstamp::string AS true_tstamp,
     uploaded_at
