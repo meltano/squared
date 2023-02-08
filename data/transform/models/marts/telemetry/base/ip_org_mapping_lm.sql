d44707b69a2f533346e5743181759668b5078659
pnadolny13 <patnadolny@gmail.com>
2023-02-08 15:58:02 -0500

    fix sqlfluff errors

--- data/transform/models/marts/telemetry/base/ip_org_mapping_lm.sql
+++ data/transform/models/marts/telemetry/base/ip_org_mapping_lm.sql
@@ -5,16 +5,16 @@ WITH base AS (
         context_identify.leadmagic_company_domain,
         ROW_NUMBER() OVER (
             PARTITION BY
-                event_id
-            ORDER BY event_created_at DESC
+                stg_snowplow__events.event_id
+            ORDER BY stg_snowplow__events.event_created_at DESC
         ) AS row_num
     FROM {{ ref('stg_snowplow__events') }}
     LEFT JOIN {{ ref('context_identify') }}
         ON stg_snowplow__events.event_id = context_identify.event_id
     WHERE context_identify.leadmagic_company_name IS NOT NULL
 )
-SELECT
-    DISTINCT
+
+SELECT DISTINCT
     ip_address_hash,
     leadmagic_company_name,
     leadmagic_company_domain
