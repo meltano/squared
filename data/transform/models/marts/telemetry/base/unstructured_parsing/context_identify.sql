00950a2e7438bdd561c2064c8d3428bf79f3404d
pnadolny13 <patnadolny@gmail.com>
2023-02-08 10:04:23 -0500

    fix sqlfluff formatting

--- data/transform/models/marts/telemetry/base/unstructured_parsing/context_identify.sql
+++ data/transform/models/marts/telemetry/base/unstructured_parsing/context_identify.sql
@@ -3,8 +3,12 @@ WITH base AS (
     SELECT
         event_id,
         MAX(schema_name) AS schema_name,
-        MAX(context:data:leadmagic_company_name::STRING) AS leadmagic_company_name,
-        MAX(context:data:leadmagic_company_domain::STRING) AS leadmagic_company_domain
+        MAX(
+            context:data:leadmagic_company_name::STRING
+        ) AS leadmagic_company_name,
+        MAX(
+            context:data:leadmagic_company_domain::STRING
+        ) AS leadmagic_company_domain
     FROM {{ ref('context_base') }}
     WHERE
         schema_name LIKE 'iglu:com.meltano/identify_context/%'
--- data/transform/tests/marts/telemetry/base/assert_all_events_parsed.sql
+++ data/transform/tests/marts/telemetry/base/assert_all_events_parsed.sql
@@ -1,8 +1,8 @@
 -- All events from event_unstruct should arrive in one of the
 -- parsed tables.
 SELECT DISTINCT event_id FROM {{ ref('event_unstruct') }}
-    -- page views are parsed by the snowplow dbt package
-    WHERE event != 'page_view'
+-- page views are parsed by the snowplow dbt package
+WHERE event != 'page_view'
 MINUS
 SELECT DISTINCT event_id FROM {{ ref('event_block') }}
 MINUS
