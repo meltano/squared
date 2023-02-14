7de2599c9fa078e7ef1bfead65e6ccfa0bf51088
Pat Nadolny <patnadolny@gmail.com>
2023-02-14 13:26:33 -0500

    Bugfix/coalesce description (#563)

--- data/transform/models/publish/slack_notifications/slack_alerts.sql
+++ data/transform/models/publish/slack_notifications/slack_alerts.sql
@@ -87,7 +87,10 @@ repos AS (
                     THEN '\n     • <'
                     || stg_github_search__repositories.repo_url || ' | '
                     || stg_github_search__repositories.repo_full_name
-                    || '> - _' || stg_github_search__repositories.description
+                    || '> - _' || COALESCE(
+                        stg_github_search__repositories.description,
+                        'No Description'
+                    )
                     || '_\n'
 
             END
