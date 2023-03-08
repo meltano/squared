121e3cbc4ca5ce342406b0113b353365737b9a35
pnadolny13 <patnadolny@gmail.com>
2023-03-08 17:11:03 -0500

    fix linting

--- data/transform/models/marts/telemetry/base/project_base.sql
+++ data/transform/models/marts/telemetry/base/project_base.sql
@@ -3,6 +3,13 @@ WITH first_values AS (
     SELECT DISTINCT
         project_id,
         FIRST_VALUE(
+            CASE WHEN is_exec_event = TRUE THEN options_obj END
+        ) IGNORE NULLS OVER (
+            PARTITION BY
+                project_id
+            ORDER BY COALESCE(started_ts, finished_ts) ASC
+        ):elt:state::STRING AS first_elt_state,
+        FIRST_VALUE(
             CASE
                 WHEN cli_command != 'init'
                     THEN project_uuid_source
@@ -30,14 +37,7 @@ WITH first_values AS (
             PARTITION BY
                 project_id
             ORDER BY COALESCE(started_ts, finished_ts) ASC
-        ) AS first_project_directory,
-        FIRST_VALUE(
-            CASE WHEN is_exec_event = TRUE THEN options_obj END
-        ) IGNORE NULLS OVER (
-            PARTITION BY
-                project_id
-            ORDER BY COALESCE(started_ts, finished_ts) ASC
-        ):elt:state::STRING AS first_elt_state
+        ) AS first_project_directory
     FROM {{ ref('cli_executions_base') }}
 
 ),
@@ -234,7 +234,7 @@ plugin_aggregates AS (
 project_aggregates AS (
     SELECT
         cli_executions_base.project_id,
-        ARRAY_AGG(DISTINCT ip_address_hash) as ip_hash_list,
+        ARRAY_AGG(DISTINCT cli_executions_base.ip_address_hash) AS ip_hash_list,
         MIN(cli_executions_base.event_created_at) AS first_event_at,
         MAX(cli_executions_base.event_created_at) AS last_event_at,
         DATEDIFF(
@@ -399,7 +399,9 @@ SELECT
         project_org_mapping.org_domain,
         'UNKNOWN'
     ) AS project_org_domain,
-    CASE WHEN first_values.first_elt_state IS NOT NULL THEN TRUE ELSE FALSE END AS is_state_in_first_exec
+    COALESCE(first_values.first_elt_state IS NOT NULL,
+        FALSE
+    ) AS is_state_in_first_exec
 FROM project_aggregates
 LEFT JOIN
     first_values ON
