fec37cd0ab440aeac3e8709b777dd4af49580e48
pnadolny13 <patnadolny@gmail.com>
2023-04-12 18:40:29 -0400

    sqlfluff fixes

--- data/transform/models/staging/dynamodb/stg_dynamodb__workload_metadata_table.sql
+++ data/transform/models/staging/dynamodb/stg_dynamodb__workload_metadata_table.sql
@@ -12,31 +12,31 @@ WITH source AS (
 renamed AS (
 
     SELECT
-        ecs_task_status,
-        NULLIF(end_time, 'N/A')::TIMESTAMP_TZ AS end_time_ts,
-        execution_id AS cloud_execution_id,
-        NULLIF(exit_code, 'N/A')::INT AS cloud_exit_code,
-        NULLIF(start_time, 'N/A')::TIMESTAMP_TZ AS start_time_ts,
-        ttl::INT AS cloud_run_ttl,
-        SHA2_HEX(command_text) AS command_text_hash,
+        source.ecs_task_status,
+        NULLIF(source.end_time, 'N/A')::TIMESTAMP_TZ AS end_time_ts,
+        source.execution_id AS cloud_execution_id,
+        NULLIF(source.exit_code, 'N/A')::INT AS cloud_exit_code,
+        NULLIF(source.start_time, 'N/A')::TIMESTAMP_TZ AS start_time_ts,
+        source.ttl::INT AS cloud_run_ttl,
+        SHA2_HEX(source.command_text) AS command_text_hash,
         COALESCE(
             hash_lookup.unhashed_value,
             SHA2_HEX(source.environment_name)
         ) AS cloud_environment_name,
-        SHA2_HEX(job_name) AS cloud_job_name_hash,
-        SHA2_HEX(schedule_name) AS cloud_schedule_name_hash,
+        SHA2_HEX(source.job_name) AS cloud_job_name_hash,
+        SHA2_HEX(source.schedule_name) AS cloud_schedule_name_hash,
         SPLIT_PART(
-            "TENANT_RESOURCE_KEY::PROJECT_ID", '::', 1
+            source."TENANT_RESOURCE_KEY::PROJECT_ID", '::', 1 -- noqa: RF05
         ) AS surrogate_tenant_resource_key,
         SPLIT_PART(
-            "TENANT_RESOURCE_KEY::PROJECT_ID", '::', 2
+            source."TENANT_RESOURCE_KEY::PROJECT_ID", '::', 2 -- noqa: RF05
         ) AS cloud_project_id
     FROM source
     LEFT JOIN {{ ref('hash_lookup') }}
-    ON
-        SHA2_HEX(source.environment_name) = hash_lookup.hash_value
-        AND hash_lookup.category = 'environment'
-    WHERE row_num = 1
+        ON
+            SHA2_HEX(source.environment_name) = hash_lookup.hash_value
+            AND hash_lookup.category = 'environment'
+    WHERE source.row_num = 1
 
 )
 
