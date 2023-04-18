91717237d5c42e9d8dcc6f7aa9eb00937e11bca5
pnadolny13 <patnadolny@gmail.com>
2023-04-18 15:43:26 -0400

    reorder pipeline_dim for sqlfluff

--- data/transform/models/marts/telemetry/pipeline_dim.sql
+++ data/transform/models/marts/telemetry/pipeline_dim.sql
@@ -2,6 +2,6 @@ SELECT DISTINCT
     pipeline_pk,
     project_id,
     env_id,
-    ARRAY_SIZE(plugins) AS plugin_count,
-    is_test_pipeline
+    is_test_pipeline,
+    ARRAY_SIZE(plugins) AS plugin_count
 FROM {{ ref('pipeline_executions') }}
