fe1ac8612de5e3efdfffa260e4f173b5bcc619ab
pnadolny13 <patnadolny@gmail.com>
2023-05-12 14:58:39 -0400

    fix tests using row number instead of rank

--- data/transform/snapshots/spreadsheets_anywhere/snapshot_ssa_gcp_ips.sql
+++ data/transform/snapshots/spreadsheets_anywhere/snapshot_ssa_gcp_ips.sql
@@ -20,7 +20,7 @@
         FROM {{ source('tap_spreadsheets_anywhere', 'gcp_ips') }}
         -- Handle hard deletes by only selecting most recent sync
         QUALIFY
-            RANK() OVER (
+            ROW_NUMBER() OVER (
                 ORDER BY DATE_TRUNC('MINUTE', _sdc_batched_at) DESC
             ) = 1
 
