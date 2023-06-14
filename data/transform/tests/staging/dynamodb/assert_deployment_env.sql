c921a99b1e95290a29d0d823e3ad4a4f9944c4da
pnadolny13 <patnadolny@gmail.com>
2023-06-14 08:43:45 -0400

    uncomment and disable instead of commenting out

--- data/transform/tests/staging/dynamodb/assert_deployment_env.sql
+++ data/transform/tests/staging/dynamodb/assert_deployment_env.sql
@@ -1,6 +1,11 @@
+{{
+    config(
+        enabled=false
+    )
+}}
 -- This will alert us when deployments dont match env names which will
 -- break the metadata + project schedules join
 -- TODO: refactor joins https://github.com/meltano/squared/issues/643
--- SELECT *
--- FROM {{ ref('stg_dynamodb__project_deployments') }}
--- WHERE cloud_deployment_name_hash != cloud_environment_name_hash
+SELECT *
+FROM {{ ref('stg_dynamodb__project_deployments') }}
+WHERE cloud_deployment_name_hash != cloud_environment_name_hash
