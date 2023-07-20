69f07b3ccd3e22193419a22ecfa28a206fe9c9d0
pnadolny13 <patnadolny@gmail.com>
2023-07-20 16:40:45 -0400

    use schedules data from the deployments table

--- data/transform/models/staging/dynamodb/stg_dynamodb__project_schedules_table.sql
+++ data/transform/models/staging/dynamodb/stg_dynamodb__project_schedules_table.sql
@@ -1,54 +1,19 @@
-WITH source AS (
-
-    SELECT
-        *,
-        SPLIT_PART(
-            "DEPLOYMENT_NAME::SCHEDULE_NAME", '::', 1 -- noqa: RF05
-        ) AS cloud_deployment_name,
-        REPLACE(
-            SPLIT_PART(
-                "DEPLOYMENT_NAME::SCHEDULE_NAME", -- noqa: RF05
-                '::',
-                2
-            ), '-', '_'
-        ) AS cloud_schedule_name,
-        SPLIT_PART(
-            "TENANT_RESOURCE_KEY::PROJECT_ID", '::', 1 -- noqa: RF05
-        ) AS tenant_resource_key,
-        SPLIT_PART(
-            "TENANT_RESOURCE_KEY::PROJECT_ID", '::', 2 -- noqa: RF05
-        ) AS cloud_project_id
-    FROM {{ source('tap_dynamodb', 'project_schedules_table') }}
-),
-
-clean_source AS (
-    SELECT
-        *,
-        {{ dbt_utils.surrogate_key(
-            ['tenant_resource_key', 'cloud_project_id', 'cloud_deployment_name', 'cloud_schedule_name']
-        ) }} AS schedule_surrogate_key,
-        ROW_NUMBER() OVER (
-            PARTITION BY schedule_surrogate_key
-            ORDER BY CAST(_sdc_batched_at AS TIMESTAMP_NTZ) DESC
-        ) AS row_num
-    FROM source
-),
-
-renamed AS (
-
-    SELECT
-        schedule_surrogate_key,
-        interval,
-        enabled AS is_enabled,
-        tenant_resource_key,
-        cloud_project_id,
-        eventbridge_name,
-        SHA2_HEX(cloud_deployment_name) AS cloud_deployment_name_hash,
-        SHA2_HEX(cloud_schedule_name) AS cloud_schedule_name_hash
-    FROM clean_source
-    WHERE row_num = 1
-
-)
-
-SELECT *
-FROM renamed
+SELECT
+    {{ dbt_utils.surrogate_key(
+        [
+            'stg_dynamodb__project_deployments_base.deployment_surrogate_key',
+            'CAST(schedule_elem.value:schedule_name AS STRING)'
+        ]
+    ) }} AS schedule_surrogate_key,
+    stg_dynamodb__project_deployments_base.deployment_surrogate_key,
+    CAST(schedule_elem.value:interval AS STRING) AS schedule_interval,
+    CAST(schedule_elem.value:enabled AS BOOLEAN) AS schedule_is_enabled,
+    stg_dynamodb__project_deployments_base.tenant_resource_key,
+    stg_dynamodb__project_deployments_base.cloud_project_id,
+    CAST(schedule_elem.value:eventbridge_name AS STRING) AS eventbridge_name,
+    stg_dynamodb__project_deployments_base.cloud_deployment_name_hash,
+    SHA2_HEX(
+        CAST(schedule_elem.value:schedule_name AS STRING)
+    ) AS cloud_schedule_name_hash
+FROM {{ ref('stg_dynamodb__project_deployments_base') }},
+    LATERAL FLATTEN(input => schedules_obj) AS schedule_elem
