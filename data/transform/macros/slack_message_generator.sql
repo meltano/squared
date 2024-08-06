849970c45c883b85579e390b45ff71640388f818
Pat Nadolny <patnadolny@gmail.com>
2024-08-06 12:42:34 -0400

    fix: temporarily remove github author names from slack notifications (#686)

--- data/transform/macros/slack_message_generator.sql
+++ data/transform/macros/slack_message_generator.sql
@@ -1,3 +1,3 @@
 {% macro slack_message_generator() -%}
-'\n     • <' || html_url || ' | ' || organization_name || '/' || repo_name || ' #' || GET(split(html_url, '/'),  array_size(split(html_url, '/'))-1)::STRING || '> (*' || author_username || '*) - _' || title || '_'  || CASE WHEN singer_contributions.is_hub_listed THEN ' (:melty-flame: < ' || hub_unique.docs || ' | _MeltanoHub Link_>)\n' ELSE '\n' END
+'\n     • <' || html_url || ' | ' || organization_name || '/' || repo_name || ' #' || GET(split(html_url, '/'),  array_size(split(html_url, '/'))-1)::STRING || '> - _' || title || '_'  || CASE WHEN singer_contributions.is_hub_listed THEN ' (:melty-flame: < ' || hub_unique.docs || ' | _MeltanoHub Link_>)\n' ELSE '\n' END
 {%- endmacro %}
